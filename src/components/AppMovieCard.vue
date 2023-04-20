52595a43fc9473c717ba7427821fdc9ec2734c89
Cris-NTL <natolicristian92@gmail.com>
2023-04-20 15:09:51 +0200

    Add: flag-icons(needs refactoring)

--- src/components/AppMovieCard.vue
+++ src/components/AppMovieCard.vue
@@ -4,8 +4,71 @@ export default {
     props: {
         movie: Object,
     },
-
-}
+    computed: {
+        flagIconClass() {
+            return `fi-${this.getCountryCode(this.movie.original_language)}`;
+        },
+    },
+    methods: {
+        getCountryCode(languageCode) {
+            const languageToCountry = {
+                af: 'za',
+                ar: 'sa',
+                bg: 'bg',
+                bn: 'bd',
+                ca: 'es',
+                cs: 'cz',
+                cy: 'gb',
+                da: 'dk',
+                de: 'de',
+                el: 'gr',
+                en: 'gb',
+                es: 'es',
+                et: 'ee',
+                fa: 'ir',
+                fi: 'fi',
+                fr: 'fr',
+                gu: 'in',
+                he: 'il',
+                hi: 'in',
+                hr: 'hr',
+                hu: 'hu',
+                id: 'id',
+                it: 'it',
+                ja: 'jp',
+                kn: 'in',
+                ko: 'kr',
+                lt: 'lt',
+                lv: 'lv',
+                ml: 'in',
+                mr: 'in',
+                ms: 'my',
+                nl: 'nl',
+                no: 'no',
+                pa: 'in',
+                pl: 'pl',
+                pt: 'pt',
+                ro: 'ro',
+                ru: 'ru',
+                sk: 'sk',
+                sl: 'si',
+                sq: 'al',
+                sr: 'rs',
+                sv: 'se',
+                ta: 'in',
+                te: 'in',
+                th: 'th',
+                tr: 'tr',
+                uk: 'ua',
+                ur: 'pk',
+                vi: 'vn',
+                zh: 'cn',
+                zu: 'za',
+            };
+            return languageToCountry[languageCode] || languageCode;
+        },
+    },
+};
 </script>
 
 <template>
@@ -13,9 +76,10 @@ export default {
         <img :src="`https://image.tmdb.org/t/p/w342${movie.poster_path}`" class="card-img-top" alt="Movie poster" />
         <div class="card-body">
             <h5 class="card-title">{{ movie.title }}</h5>
-            <p class="card-text">{{ movie.original_title }}</p>
-            <p class="card-text">{{ movie.original_language }}</p>
-            <span class="fi fi-gr"></span> <span class="fi fi-gr fis"></span>
+            <h6 class="card-text">{{ movie.original_title }}</h6>
+            <p class="card-text">
+                <span :class="['fi', flagIconClass]"></span>
+            </p>
             <p class="card-text">{{ movie.vote_average }}</p>
         </div>
     </div>
--- src/components/AppSeriesCard.vue
+++ src/components/AppSeriesCard.vue
@@ -4,7 +4,70 @@ export default {
     props: {
         series: Object,
     },
-
+    computed: {
+        flagIconClass() {
+            return `fi-${this.getCountryCode(this.series.original_language)}`;
+        },
+    },
+    methods: {
+        getCountryCode(languageCode) {
+            const languageToCountry = {
+                af: 'za',
+                ar: 'sa',
+                bg: 'bg',
+                bn: 'bd',
+                ca: 'es',
+                cs: 'cz',
+                cy: 'gb',
+                da: 'dk',
+                de: 'de',
+                el: 'gr',
+                en: 'gb',
+                es: 'es',
+                et: 'ee',
+                fa: 'ir',
+                fi: 'fi',
+                fr: 'fr',
+                gu: 'in',
+                he: 'il',
+                hi: 'in',
+                hr: 'hr',
+                hu: 'hu',
+                id: 'id',
+                it: 'it',
+                ja: 'jp',
+                kn: 'in',
+                ko: 'kr',
+                lt: 'lt',
+                lv: 'lv',
+                ml: 'in',
+                mr: 'in',
+                ms: 'my',
+                nl: 'nl',
+                no: 'no',
+                pa: 'in',
+                pl: 'pl',
+                pt: 'pt',
+                ro: 'ro',
+                ru: 'ru',
+                sk: 'sk',
+                sl: 'si',
+                sq: 'al',
+                sr: 'rs',
+                sv: 'se',
+                ta: 'in',
+                te: 'in',
+                th: 'th',
+                tr: 'tr',
+                uk: 'ua',
+                ur: 'pk',
+                vi: 'vn',
+                zh: 'cn',
+                zu: 'za',
+            };
+            return languageToCountry[languageCode] || languageCode;
+        },
+    },
 }
 </script>
 
@@ -13,9 +76,10 @@ export default {
         <img :src="`https://image.tmdb.org/t/p/w342${series.poster_path}`" class="card-img-top" alt="Series poster" />
         <div class="card-body">
             <h5 class="card-title">{{ series.name }}</h5>
-            <p class="card-text">{{ series.original_title }}</p>
-            <p class="card-text">{{ series.original_language }}</p>
-            <span class="fi fi-gr"></span> <span class="fi fi-gr fis"></span>
+            <h6 class="card-text">{{ series.original_title }}</h6>
+            <p class="card-text">
+                <span :class="['fi', flagIconClass]"></span>
+            </p>
             <p class="card-text">{{ series.vote_average }}</p>
         </div>
     </div>
